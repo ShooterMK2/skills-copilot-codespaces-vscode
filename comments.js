5513c4a3023ea22b9ccfd19b26aeff2e20205030
ShooterMK2 <82992086+ShooterMK2@users.noreply.github.com>
2025-07-26 16:09:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -5,12 +5,12 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.end('Hello World!!\n');
 });
 server.listen(port, () => {     
     console.log(`Server running at http://localhost:${port}/`);
 });
-// This is a simple web server that listens on port 3000 and responds with "Hello World" to any request.
+// This is a simple web server that listens on port 3000 and responds with "Hello World!!" to any request
 
 
 
